7d39ca2e2bdf130a1bf62eeb9fd6b33600e2fa33
Joshua <mwalejoshua89@gmail.com>
2022-06-30 16:09:21 +0300

    commit tree 8f17f78744218dd5c56aecdc4f6910ae803ccb3c

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function scuberGreetingForFeet(feet){
-  if (feet <= 400){
-    return "This one is on me!"
+function scuberGreetingForFeet(feet) {
+  if (feet <=400) {
+    return 'This one is on me!'
   }
-  else if (feet >= 2000 && feet <2500) {
-    return "I will gladly take your thirty bucks."
+  if (feet >=2000 && feet <2500) {
+  return 'I will gladly take your thirty bucks.'
   }
-  else if (feet > 2500)
-  return "No can do."
+  if (feet > 2500) {
+    return 'No can do.'
   }
-  
-
-function ternaryCheckCity(city){
-  return city ==="NYC"?"Ok, sounds good.":"No go.";
-  // Write your code here!
 }
 
+ function ternaryCheckCity(city) {
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(){
+  // Write your code here!
 function switchOnCharmFromTip(Tip){
   switch(Tip) {
     case "generous":
-    return "Thank you so much."
+    return 'Thank you so much.'
     break;
     case "not as generous":
-    return "Thank you."
+    return 'Thank you.'
     break;
-    case "thanks for everything":
-    return "Bye."
-    break;
-    
+    default:
+    return 'Bye.'
   }
+}
 }
